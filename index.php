fb0e19786c707f497b802a5b52d4b723cf5c9bfa
Jace Poirier-Pinto <jace@Jacezilla.(none)>
2013-07-28 20:00:15 -0700

    pushing index

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Custom AddThis
+Plugin URI: http://rabble.com
+Version: 1.0
+Description: Custom pluging to controll AddThis buttons
+Author: Jace Poirier-Pint
+Author URI: http://jacewdim393f.wordpress.com
+License: GNU General Public License v2 or later
+*/
+
