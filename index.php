6c3ee1665a2232ef26a4fe7d0828dca0accfd981
Jace Poirier-Pinto <jace@Jacezilla.(none)>
2013-07-28 22:56:51 -0700

    I finally got it working, it's a little "janky" but I got it to work :P

--- index.php
+++ index.php
@@ -22,18 +22,20 @@ function jkp_enqueue_script() {
 }
 add_action('wp_enqueue_scripts', 'jkp_enqueue_script');
  
-function addThis_option_script($num_preferred, $position) {
+function addThis_option_script() {
+  if ('0' === get_option('jkp_disable_button', '0')) {
 ?>
   <script type="text/javascript">
     addthis.layers({
       'theme' : 'transparent',
       'share' : {
-        'position' : 'left',
-        'numPreferredServices' : 5
+        'position' : <?php echo "'". get_option('jkp_position', 'left') ."'";  ?>,
+        'numPreferredServices' : <?php echo get_option('jkp_num_preferred', 4) ?>
       }   
     });
   </script>
 <?php
+  }
 }
 add_action('wp_footer', 'addThis_option_script');
 
@@ -54,7 +56,7 @@ function jkp_render_options_page() {
     <?php screen_icon(); ?>
     <h2><?php _e('JKP Options'); ?></h2>
     <form action="options.php" method="post">
-      <?php settings_fields('jkp_disable_button'); ?>
+      <?php settings_fields('jkp_option_group'); ?>
       <?php do_settings_sections('jkp_options_page'); ?>
       <p class="submit">
         <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save Changes'); ?>">
@@ -66,9 +68,20 @@ function jkp_render_options_page() {
 
 function jkp_add_addThis_settings() {
   register_setting(
-    'jkp_disable_button',
+    'jkp_option_group',
     'jkp_disable_button',
     'absint'
+  );
+
+  register_setting(
+    'jkp_option_group',
+    'jkp_num_preferred',
+    'absint'
+  );
+
+  register_setting(
+    'jkp_option_group',
+    'jkp_position'
   );
 
   add_settings_section(
@@ -110,13 +123,23 @@ function jkp_render_main_settings_section() {
 
 function jkp_render_disable_button_input() {
   $current = get_option('jkp_disable_button', 0);
-  echo '<input id="jkp-disable-button" name="jkp_disable_button" type="checkbox" value="1" ' . checked( 1, $current, false ) . ' />';
+  echo '<input id="jkp-disable-button" name="jkp_disable_button" type="checkbox" value="1"'. checked(1, $current, false) .'/>';
 }
 
 function jkp_render_num_preferred_input() {
-  // num preferred input field
+  $current = get_option('jkp_num_preferred', 4);
+  echo '<label><input type="radio" name="jkp_num_preferred" value="1"'. checked(1, $current, false) .'/> 1 </label>';
+  echo '<label><input type="radio" name="jkp_num_preferred" value="2"'. checked(2, $current, false) .'/> 2 </label>';
+  echo '<label><input type="radio" name="jkp_num_preferred" value="3"'. checked(3, $current, false) .'/> 3 </label>';
+  echo '<label><input type="radio" name="jkp_num_preferred" value="4"'. checked(4, $current, false) .'/> 4 </label>';
+  echo '<label><input type="radio" name="jkp_num_preferred" value="5"'. checked(5, $current, false) .'/> 5 </label>';
+  echo '<label><input type="radio" name="jkp_num_preferred" value="6"'. checked(6, $current, false) .'/> 6 </label>';
 }
 
 function jkp_render_position_input() {
-  // position input field
+  $current = get_option('jkp_position', 'left');
+  echo '<select name="jkp_position">';
+  echo '<option value="left"'. selected('left', $current, false) .'>Left</option>';
+  echo '<option value="right"'. selected('right', $current, false) .'>Right</option>';
+  echo '</select>';
 }
